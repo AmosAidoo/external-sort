e0eeb0287ead1acfa2d447587888a53c0e53560d
Amos Aidoo <aidooamos20@gmail.com>
2025-02-05 14:25:25 +0100

    source and header for external_sort function to be implemented

---
+++ include/sort.h
@@ -0,0 +1,9 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <cstdint>
+namespace extsort {
+  void external_sort(int fd_input, uint64_t size, int fd_output, uint64_t mem_size);
+}
+
+#endif // SORT_H
---
+++ src/sort.cpp
@@ -0,0 +1,8 @@
+#include "sort.h"
+#include <cstdint>
+
+namespace extsort {
+  void external_sort(int fd_input, uint64_t size, int fd_output, uint64_t mem_size) {
+    // Implementation comes here
+  }
+}
